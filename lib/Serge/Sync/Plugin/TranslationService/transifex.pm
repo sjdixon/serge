85216e099f9dd0a88b637a9efab67af6166929a7
Dragos Varovici <dvarovici.work@gmail.com>
2018-01-27 19:58:17 -0700

    Transifex plugin root directory support

--- lib/Serge/Sync/Plugin/TranslationService/transifex.pm
+++ lib/Serge/Sync/Plugin/TranslationService/transifex.pm
@@ -3,6 +3,7 @@ use parent Serge::Sync::Plugin::Base::TranslationService, Serge::Interface::SysC
 
 use strict;
 
+use File::chdir;
 use Serge::Util qw(subst_macros);
 
 sub name {
@@ -15,22 +16,41 @@ sub init {
     $self->SUPER::init(@_);
 
     $self->{optimizations} = 1; # set to undef to disable optimizations
+
+    $self->merge_schema({
+        root_directory => 'STRING'
+    });
 }
 
 sub validate_data {
     my ($self) = @_;
 
     $self->SUPER::validate_data;
+
+    $self->{data}->{root_directory} = subst_macros($self->{data}->{root_directory});
+    $self->{data}->{push_translations} = subst_macros($self->{data}->{push_translations});
+
+    die "'root_directory' not defined" unless defined $self->{data}->{root_directory};
+    die "'root_directory', which is set to '$self->{data}->{root_directory}', does not point to a valid file.\n" unless -d $self->{data}->{root_directory};
 }
 
 sub run_transifex_cli {
     my ($self, $action, $langs, $capture) = @_;
 
+    my $cli_return = 0;
+
     my $command = $action;
 
     $command = 'tx '.$command;
     print "Running '$command'...\n";
-    return $self->run_cmd($command, $capture);
+
+    {
+        local $CWD = $self->{data}->{root_directory};
+
+        $cli_return = $self->run_cmd($command, $capture);
+    }
+
+    return $cli_return;
 }
 
 sub pull_ts {
@@ -42,7 +62,7 @@ sub pull_ts {
 sub push_ts {
     my ($self, $langs) = @_;
 
-    $self->run_transifex_cli('push -s', $langs);
+    return $self->run_transifex_cli('push --source', $langs);;
 }
 
 1;
