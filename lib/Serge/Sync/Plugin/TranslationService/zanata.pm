b1ec74bd8b692f05f6abc61ff27ea1799ba0722a
Dragos Varovici <dvarovici.work@gmail.com>
2018-06-05 09:11:55 -0600

    Set version to 0.9 (matching published cpan version)

--- lib/Serge/Sync/Plugin/TranslationService/zanata.pm
+++ lib/Serge/Sync/Plugin/TranslationService/zanata.pm
@@ -1,9 +1,14 @@
+# ABSTRACT: Serge Zanata translation server (http://zanata.org/) synchronization plugin
+
 package Serge::Sync::Plugin::TranslationService::zanata;
+
 use parent Serge::Sync::Plugin::Base::TranslationService, Serge::Interface::SysCmdRunner;
 
 use strict;
 
 use Serge::Util qw(subst_macros);
+
+our $VERSION = '0.9';
 
 sub name {
     return 'Zanata translation server (http://zanata.org/) synchronization plugin';
@@ -68,7 +73,7 @@ sub run_zanata_cli {
     }
 
     $command = 'zanata-cli '.$command;
-    
+
     print "Running '$command'...\n";
     return $self->run_cmd($command, $capture);
 }
